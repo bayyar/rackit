5f36f4ec1af8b79ae9d5e195e7b3759e8d372e1a
Bharat Ayyar <bayyar@gmail.com>
2014-04-24 18:00:22 -0400

    Change styling on User#show, updates products CSV, makes slight add to feeds controller to get closer on AR query, adds products.csv to gitignore (not working for some reason)

--- app/controllers/users/feeds_controller.rb
+++ app/controllers/users/feeds_controller.rb
@@ -4,6 +4,8 @@ def index
 	@user = User.where(username: params[:username]).first
 	# Need to fix this
 	@preferences = UserProductPreference.all.where(is_racked: true).limit(27)
+	# Alternative that is closer to the eventual one.
+	# @preferences = Product.joins(:user_product_preferences).where({ user_product_preferences: { is_racked: true }})
 	@following = @user.following
   @followers = User.followers(@user)
 end
