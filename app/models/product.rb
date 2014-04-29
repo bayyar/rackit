aa48b8b4923639ebf487ed6f8d289a2cf7be14bb
Bharat Ayyar <bayyar@gmail.com>
2014-04-29 18:33:19 -0400

    Work on AR querying for Feed and Popular

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,4 +12,20 @@ class Product < ActiveRecord::Base
 		where("products.id NOT IN (SELECT user_product_preferences.product_id from user_product_preferences where user_product_preferences.user_id = ?)", user_id).order('random()').limit(1).first
 	end
 
+	def self.rack(user)
+		Product.joins(:user_product_preferences).where({ user_product_preferences: { is_racked: true, user_id: user.id }})
+	end
+
+	# def self.feed(user)
+	# 	UserProductPreference.where({ user_product_preferences: { is_racked: true }}).limit(33)
+	# 	# u = user.user_friendships.each do |friend|
+	# 	# 	UserProductPreference.where({ user_product_preferences: { is_racked: true, user_id: friend.friend_id }})
+	# 	# end
+	# end
+
+	# def self.global
+	# 	# How to order by "racks.count"?
+	# 	UserProductPreference.where({ user_product_preferences: { is_racked: true }}).limit(33)
+	# end
+
 end
